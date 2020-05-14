d3e3f817a02aa53bc688c0bfad6158935e017b1f
umang <umang.rastogi96@gmail.com>
2020-05-14 10:37:52 -0400

    [image_classifier.py] Call train network function from classifier

--- Code/image_classifier.py
+++ Code/image_classifier.py
@@ -1,5 +1,6 @@
 from sys import argv
 from utils.data_prep import PreProcessing
+from utils import classifier
 
 script, location = argv
 
@@ -7,3 +8,4 @@ script, location = argv
 if __name__ == '__main__':
     pre_processing = PreProcessing(location)
     pre_processing.restructure_dataset()
+    classifier.run_test_harness()
