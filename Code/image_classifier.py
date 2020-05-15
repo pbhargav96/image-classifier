0bc9219cc95f3563da94a7dfb66103f2a33aca73
umang <umang.rastogi96@gmail.com>
2020-05-15 12:36:00 -0400

    [image_classifier.py] Use classifier class

--- Code/image_classifier.py
+++ Code/image_classifier.py
@@ -1,6 +1,6 @@
 from sys import argv
 from utils.data_prep import PreProcessing
-from utils import classifier
+from utils.classifier import Classifier
 
 script, location = argv
 
@@ -8,4 +8,5 @@ script, location = argv
 if __name__ == '__main__':
     pre_processing = PreProcessing(location)
     pre_processing.restructure_dataset()
+    classifier = Classifier(location)
     classifier.run_test_harness()
